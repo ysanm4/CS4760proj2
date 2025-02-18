3927dfcfa96ee29b767a8e49cc9860c74a249fd7
yosef alqufidi <alqufidi@opsys.cs.umsl.edu>
2025-02-18 14:16:46 -0600

    third commit

--- worker.cpp
+++ worker.cpp
@@ -14,19 +14,61 @@ using namespace std;
 struct ClockDigi{
 	int sysClocks;
 	int sysClockNano;
-	int yermTimeS;
-	int termTimeNano;
 };
 
 //logic for shared memory
-//
-//
-//
+
+int main(int argc, char** argv){
+	if(argc !=3){
+		cout<<"Error please use three arguments for:"<< argv[0] <<"\n";
+		return EXIT_FAILURE;
+	}
 
 
-//start capturing
+//start parseing time
 
-int startCount = clockDigi->sysClocks;
+int Secval = atoi(argv[1]);
+int Nanoval = atoi(argv[2]);
+
+//shared memory key
+
+const char* ftokPath = "/tmp";
+key_t key = ftok(ftokPath, 'R');
+if(key == -1){
+perror("ftok");
+return EXIT_FAILURE;
+}
+
+//access to shared memory
+
+int shmid = shmget(key, sizeof(ClockDigi), 0666);
+if(shmid < 0){
+	perror("shmget");
+	return EXIT_FAILURE;
+}
+
+ClockDigi* clockVal = (ClockDigi*) shmat(shmid, nullptr, 0);
+if (clockVal == (void*) -1){
+	perror("shmat");
+	return EXIT_FAILURE;
+}
+
+//start reading from simulated clock 
+
+int stRdSec = clockVal->sysClockS;
+int stRdNano = clockVal->sysClockNano;
+
+//termination
+
+int termSec = stRdSec + Secval;
+int termNano = stRdNano + Nanoval;
+
+if(termNano >= 1000000000){
+	termSec += termNano / 1000000000;
+	termNano = termNano % 1000000000;
+}
+
+//outputs
 
 cout << "WORKER PID:" << getpid()
          << " PPID:" << getppid()
@@ -37,8 +79,7 @@ cout << "WORKER PID:" << getpid()
          cout << "--Just Starting" << "\n";
 
 //checks
-    while (clockData->sysClockS < startSeconds + 1) {
-    }
+  
 
     cout << "WORKER PID:" << getpid()
          << " PPID:" << getppid()
@@ -46,12 +87,11 @@ cout << "WORKER PID:" << getpid()
          << " SysclockNano:" << clockData->sysClockNano
          << " TermTimeS:" << clockData->termTimeS
          << " TermTimeNano:" << clockData->termTimeNano << "\n";
-    cout << "--1 seconds have passed since starting" << "\n";
+    cout << " seconds have passed since starting" << "\n";
 
 
     //checks
-    while (clockData->sysClockS < clockData->termTimeS) {
-         }
+   
   
     cout << "WORKER PID:" << getpid()
          << " PPID:" << getppid()
@@ -62,7 +102,7 @@ cout << "WORKER PID:" << getpid()
     cout << "--Terminating" << "\n";
 
     
-    shmdt(clockData);
+    shmdt(clockVal);
 
     return EXIT_SUCCESS;
 }
